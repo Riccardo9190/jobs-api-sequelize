32e2f24072954a71658da44b825edb6a1f34eeea
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2022-08-17 19:11:00 -0300

    Seeders

---
+++ src/database/seeders/20220809174057-seed-jobs-table.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // Getting an array of companies's id
+    const [companies] = await queryInterface.sequelize.query('SELECT id FROM companies;');
+
+    await queryInterface.bulkInsert('jobs', [{
+      title: 'Full-stack Javascript Developer',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      limit_date: '2022-03-31',
+      company_id: companies[0].id,
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      title: 'Front-end Developer (Vue.js)',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      limit_date: '2022-04-15',
+      company_id: companies[0].id,
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      title: 'Back-end Developer',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      limit_date: '2022-03-31',
+      company_id: companies[1].id,
+      created_at: new Date(),
+      updated_at: new Date()
+    }])
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('jobs', null, {});
+  }
+};
