1ea934e3256c4474caaca865f9c66305a8815d1a
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2022-08-17 19:09:35 -0300

    Controllers

---
+++ src/controllers/companies-controller.ts
@@ -0,0 +1,98 @@
+import { Request, Response } from 'express'
+import { Company } from '../models'
+
+export const companiesController = {
+  
+  // GET /companies
+  index: async (req: Request, res: Response) => {
+
+    try {
+      const companies = await Company.findAll()
+      return res.json(companies)
+
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // POST /companies
+  save: async (req: Request, res: Response) => {
+    const { name, bio, website, email } = req.body
+
+    try {
+      const company = await Company.create({
+        name,
+        bio,
+        website,
+        email,
+      })
+      return res.status(201).json(company)
+
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // GET /companies/:id
+  show: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      const company = await Company.findByPk(id, { include: 'jobs' })
+      return res.json(company)
+    
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // PUT /companies/:id
+  update: async (req: Request, res: Response) => {
+    const { id } = req.params
+    const { name, bio, website, email } = req.body
+
+    try {
+      // number of affected rows, number of updated companies (In this case, just one (position[0]))
+      const [affectedRows, companies] = await Company.update({
+        name,
+        bio,
+        website,
+        email,
+      }, {
+        where: { id },
+        returning: true 
+      })
+
+      return res.json(companies[0]) 
+      
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // DELETE /companies/:id
+  delete: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      await Company.destroy({
+        where: { id }
+      })
+
+      return res.status(204).send()
+    
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  }
+}
---
+++ src/controllers/jobsController.ts
@@ -0,0 +1,147 @@
+import { Request, Response } from 'express'
+import { Job } from '../models'
+
+const jobsController = {
+  
+  // GET /jobs
+  index: async (req: Request, res: Response) => {
+
+    try {
+      const jobs = await Job.findAll({ include: 'company' })
+      return res.json(jobs)
+
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // POST /jobs  
+  save: async (req: Request, res: Response) => {
+    const { title, description, limitDate, companyId } = req.body
+
+    try {
+      const job = await Job.create({
+        title,
+        description,
+        limitDate,
+        companyId,
+      })
+      return res.status(201).json(job)
+    
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // GET /jobs/:id
+  show: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      const job = await Job.findByPk(id, { include: ['company', 'candidates' ]})
+      const candidatesCount = await job?.countCandidates() // Before count, analise with job exists (?)
+
+      if (job === null) return res.status(404).json({ message: 'Vaga de emprego não encontrada' })
+      
+      return res.json({ ...job?.get(), candidatesCount }) // Adding candidatescount to job
+      
+      /* 
+        'get()' will return just instantiated properties of the model, because since as we are using spread operator, it'll return methods (add, remove, count, etc) that can be used with models, 
+        making unfeasible it conversion to json (where we just want table colummns)
+      */
+    
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // PUT /jobs/:id
+  update: async (req: Request, res: Response) => {
+    const { id } = req.params
+    const { title, description, limitDate, companyId } = req.body
+
+    try {
+      const [affectedRows, jobs] = await Job.update({
+        title,
+        description,
+        limitDate,
+        companyId,
+      }, {
+        where: { id },
+        returning: true
+      })
+
+      return res.json(jobs[0])
+    
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // DELETE /jobs/:id
+  delete: async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      await Job.destroy({
+        where: { id: id }
+      })
+
+      return res.status(204).send()
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // POST /jobs/:id/addCandidate
+  addCandidate: async (req: Request, res: Response) => {
+    const jobId = req.params.id
+    const { candidateId } = req.body
+
+    try {
+      const job = await Job.findByPk(jobId)
+
+      if (job === null) return res.status(404).json({ message: 'Vaga de emprego não encontrada' })
+
+      await job.addCandidate(candidateId)
+
+      return res.status(201).send()
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // POST /jobs/:id/removeCandidate
+  removeCandidate: async (req: Request, res: Response) => {
+    const jobId = req.params.id
+    const { candidateId } = req.body
+
+    try {
+      const job = await Job.findByPk(jobId)
+
+      if (job === null) return res.status(404).json({ message: 'Vaga de emprego não encontrada' })
+
+      await job.removeCandidate(candidateId)
+
+      return res.status(204).send()
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+}
+
+export { jobsController }
